bfce40d296f62d3e28791ab6f64ef37b9f9daf76
浮生絮语 <1710647252@qq.com>
2025-12-18 03:30:22 +0800

    新增ai聊天消息api

---
+++ src/api/ai/chat/message.js
@@ -0,0 +1,63 @@
+import request from '@/utils/request'
+import { fetchEventSource } from '@microsoft/fetch-event-source'
+import { getToken } from '@/utils/auth'
+
+// 获得指定对话的消息列表
+export function getChatMessageListByConversationId(id) {
+  return request({
+    url: '/ai/chat/message/list-by-conversation-id',
+    method: 'get',
+    params: { id: id }
+  })
+}
+
+// 删除指定消息
+export function deleteChatMessage(id) {
+  return request({
+    url: '/ai/chat/message/delete',
+    method: 'delete',
+    params: { id: id }
+  })
+}
+
+// 删除指定对话的消息
+export function deleteChatMessageByConversationId(id) {
+  return request({
+    url: '/ai/chat/message/delete-by-conversation-id',
+    method: 'delete',
+    params: { id: id }
+  })
+}
+
+// 发送 Stream 消息
+export function sendChatMessageStream(
+  conversationId,
+  content,
+  ctrl,
+  enableContext,
+  enableWebSearch,
+  onMessage,
+  onError,
+  onClose,
+  attachmentUrls
+) {
+  return fetchEventSource(import.meta.env.VITE_APP_BASE_API + '/ai/chat/message/send-stream', {
+    method: 'post',
+    headers: {
+      'Authorization': 'Bearer ' + getToken(),
+      'Content-Type': 'application/json'
+    },
+    openWhenHidden: true, //保持页面在后台（不可见时）的连接活跃
+    body: JSON.stringify({
+      conversationId,
+      content,
+      useContext: enableContext,
+      useSearch: enableWebSearch,
+      attachmentUrls: attachmentUrls || []
+    }),
+    onmessage: onMessage,
+    onerror: onError,
+    onclose: onClose,
+    signal: ctrl.signal
+  })
+}
