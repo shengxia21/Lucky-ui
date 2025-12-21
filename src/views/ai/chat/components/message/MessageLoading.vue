d8dc8dd7c1e7ae9becfcdb8b06678368511bcd2d
浮生絮语 <1710647252@qq.com>
2025-12-21 23:05:17 +0800

    新增消息加载组件

---
+++ src/views/ai/chat/components/message/MessageLoading.vue
@@ -0,0 +1,12 @@
+<!-- message 加载页面 -->
+<template>
+  <div class="message-loading-container">
+    <el-skeleton :rows="9" animated />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.message-loading-container {
+  padding: 30px;
+}
+</style>
