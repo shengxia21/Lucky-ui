71b98c26dcc87be187bfa977057984066acba2ed
浮生絮语 <1710647252@qq.com>
2026-01-30 00:07:09 +0800

    抽离AI聊天角色"添加/修改表单弹窗"为组件，以便复用

---
+++ src/views/ai/console/chatRole/ChatRoleForm.vue
@@ -0,0 +1,224 @@
+<template>
+  <el-dialog :title="title" v-model="open" width="500px" append-to-body>
+    <el-form ref="roleRef" :model="form" :rules="rules" label-width="90px">
+      <el-form-item label="角色名称" prop="name">
+        <el-input v-model="form.name" placeholder="请输入角色名称" />
+      </el-form-item>
+      <el-form-item label="角色头像" prop="avatar">
+        <image-upload
+          v-model="form.avatar"
+          :limit="1"
+        />
+      </el-form-item>
+      <el-form-item label="绑定模型" prop="modelId" v-if="!isUser">
+        <el-select v-model="form.modelId" placeholder="请选择模型" clearable>
+          <el-option
+            v-for="model in models"
+            :key="model.id"
+            :label="model.name"
+            :value="model.id"
+          />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="角色类别" prop="category">
+        <el-input v-model="form.category" placeholder="请输入角色类别" />
+      </el-form-item>
+      <el-form-item label="角色描述" prop="description">
+        <el-input v-model="form.description" type="textarea" placeholder="请输入角色描述" />
+      </el-form-item>
+      <el-form-item label="角色设定" prop="systemMessage">
+        <el-input v-model="form.systemMessage" type="textarea" placeholder="请输入角色设定" />
+      </el-form-item>
+      <el-form-item label="引用知识库" prop="knowledgeIds">
+        <el-select v-model="form.knowledgeIds" placeholder="请选择知识库" clearable multiple>
+          <el-option
+            v-for="item in knowledgeList"
+            :key="item.id"
+            :label="item.name"
+            :value="item.id"
+          />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="引用工具" prop="toolIds">
+        <el-select v-model="form.toolIds" placeholder="请选择工具" clearable multiple>
+          <el-option v-for="item in toolList" :key="item.id" :label="item.name" :value="item.id" />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="引用 MCP" prop="toolIds">
+        <el-select v-model="form.mcpClientNames" placeholder="请选择 MCP" clearable multiple>
+          <el-option
+            v-for="dict in ai_mcp_client_name"
+            :key="dict.value"
+            :label="dict.label"
+            :value="dict.value"
+          />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="是否公开" prop="publicStatus" v-if="!isUser">
+        <el-radio-group v-model="form.publicStatus">
+          <el-radio
+            v-for="dict in boolean_string"
+            :key="dict.value"
+            :value="dict.value"
+          >{{ dict.label }}</el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="角色排序" prop="sort" v-if="!isUser">
+        <el-input-number v-model="form.sort" :min="0" placeholder="请输入角色排序" />
+      </el-form-item>
+      <el-form-item label="开启状态" prop="status" v-if="!isUser">
+        <el-radio-group v-model="form.status">
+          <el-radio
+            v-for="dict in sys_normal_disable"
+            :key="dict.value"
+            :value="parseInt(dict.value)"
+          >{{ dict.label }}</el-radio>
+        </el-radio-group>
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button type="primary" @click="submitForm">确 定</el-button>
+        <el-button @click="cancel">取 消</el-button>
+      </div>
+    </template>
+  </el-dialog>
+</template>
+
+<script setup name="ChatRoleForm">
+import { getRole, addRole, updateRole, addMyRole, updateMyRole } from "@/api/ai/console/chatRole"
+import { getModelSimpleList } from "@/api/ai/console/model"
+import { AiModelTypeEnum } from '@/utils/constants/AiConstants'
+
+defineExpose({ openDialog }) // 提供 open 方法，用于打开弹窗
+
+const emit = defineEmits(['success']) // 定义 success 事件，用于操作成功后的回调
+
+const { proxy } = getCurrentInstance()
+const { boolean_string, sys_normal_disable, ai_mcp_client_name } = proxy.useDict('boolean_string', 'sys_normal_disable', 'ai_mcp_client_name')
+
+/** 是否【我】自己创建，私有角色 */
+const isUser = computed(() => {
+  return formType.value === 'my-create' || formType.value === 'my-update'
+})
+
+const open = ref(false)
+const title = ref('')
+const formType = ref('') // 表单的类型：create - 新增；update - 修改
+const models = ref([])  // 聊天模型列表
+const knowledgeList = ref([]) // 知识库列表
+const toolList = ref([]) // 工具列表
+
+const data = reactive({
+  form: {
+    id: null,
+    name: null,
+    avatar: null,
+    category: null,
+    description: null,
+    systemMessage: null,
+    modelId: null,
+    knowledgeIds: null,
+    toolIds: null,
+    mcpClientNames: null,
+    publicStatus: 'true',
+    sort: 1,
+    status: 0
+  },
+  rules: {
+    name: [{ required: true, message: '角色名称不能为空', trigger: 'blur' }],
+    avatar: [{ required: true, message: '角色头像不能为空', trigger: 'blur' }],
+    category: [{ required: true, message: '角色类别不能为空', trigger: 'blur' }],
+    sort: [{ required: true, message: '角色排序不能为空', trigger: 'blur' }],
+    description: [{ required: true, message: '角色描述不能为空', trigger: 'blur' }],
+    systemMessage: [{ required: true, message: '角色设定不能为空', trigger: 'blur' }],
+    publicStatus: [{ required: true, message: '是否公开不能为空', trigger: 'blur' }]
+  }
+})
+
+const { form, rules } = toRefs(data)
+
+/** 新增/修改按钮操作 */
+function openDialog(type, id) {
+  reset()
+  formType.value = type
+  title.value = id ? '修改AI 聊天角色' : '新增AI 聊天角色'
+  if (id) {
+    getRole(id).then(response => {
+      form.value = response.data
+      form.value.publicStatus = form.value.publicStatus.toString()
+    })
+  }
+  // 获得下拉数据
+  getModelSimpleList(AiModelTypeEnum.CHAT).then(response => {
+    models.value = response.data
+  })
+  // 获取知识库列表
+  // 获取工具列表
+  open.value = true
+}
+
+// 取消按钮
+function cancel() {
+  open.value = false
+  reset()
+}
+
+// 表单重置
+function reset() {
+  form.value = {
+    id: null,
+    name: null,
+    avatar: null,
+    category: null,
+    description: null,
+    systemMessage: null,
+    modelId: null,
+    knowledgeIds: null,
+    toolIds: null,
+    mcpClientNames: null,
+    publicStatus: 'true',
+    sort: 1,
+    status: 0
+  }
+  proxy.resetForm("roleRef")
+}
+
+/** 提交按钮 */
+function submitForm() {
+  proxy.$refs["roleRef"].validate(valid => {
+    if (valid) {
+      if (form.value.id != null) {
+        if (formType.value === 'my-update') {
+          updateMyRole(form.value).then(response => {
+            proxy.$modal.msgSuccess("修改成功")
+            open.value = false
+            emit('success')
+          })
+        } else {
+          updateRole(form.value).then(response => {
+            proxy.$modal.msgSuccess("修改成功")
+            open.value = false
+            emit('success')
+          })
+        }
+      } else {
+        if (formType.value === 'my-create') {
+          addMyRole(form.value).then(response => {
+            proxy.$modal.msgSuccess("新增成功")
+            open.value = false
+            emit('success')
+          })
+        } else {
+          addRole(form.value).then(response => {
+            proxy.$modal.msgSuccess("新增成功")
+            open.value = false
+            emit('success')
+          })
+        }
+      }
+    }
+  })
+}
+
+</script>
--- src/views/ai/console/chatRole/index.vue
+++ src/views/ai/console/chatRole/index.vue
@@ -39,7 +39,7 @@
           type="primary"
           plain
           icon="Plus"
-          @click="openDialog('create', null, '新增AI 聊天角色')"
+          @click="openForm('create', null)"
           v-hasPermi="['ai:chat-role:create']"
         >新增</el-button>
       </el-col>
@@ -82,7 +82,7 @@
       <el-table-column label="角色排序" align="center" prop="sort" />
       <el-table-column label="操作" align="center" class-name="small-padding fixed-width" width="180" fixed="right">
         <template #default="scope">
-          <el-button link type="primary" icon="Edit" @click="openDialog('update', scope.row.id, '修改AI 聊天角色')" v-hasPermi="['ai:chat-role:update']">修改</el-button>
+          <el-button link type="primary" icon="Edit" @click="openForm('update', scope.row.id)" v-hasPermi="['ai:chat-role:update']">修改</el-button>
           <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)" v-hasPermi="['ai:chat-role:delete']">删除</el-button>
         </template>
       </el-table-column>
@@ -96,139 +96,35 @@
       @pagination="getList"
     />
 
-    <!-- 添加或修改AI 聊天角色对话框 -->
-    <el-dialog :title="title" v-model="open" width="500px" append-to-body>
-      <el-form ref="roleRef" :model="form" :rules="rules" label-width="90px">
-        <el-form-item label="角色名称" prop="name">
-          <el-input v-model="form.name" placeholder="请输入角色名称" />
-        </el-form-item>
-        <el-form-item label="角色头像" prop="avatar">
-          <image-upload
-            v-model="form.avatar"
-            :limit="1"
-          />
-        </el-form-item>
-        <el-form-item label="绑定模型" prop="modelId" v-if="!isUser">
-          <el-select v-model="form.modelId" placeholder="请选择模型" clearable>
-            <el-option
-              v-for="model in models"
-              :key="model.id"
-              :label="model.name"
-              :value="model.id"
-            />
-          </el-select>
-        </el-form-item>
-        <el-form-item label="角色类别" prop="category">
-          <el-input v-model="form.category" placeholder="请输入角色类别" />
-        </el-form-item>
-        <el-form-item label="角色描述" prop="description">
-          <el-input v-model="form.description" type="textarea" placeholder="请输入角色描述" />
-        </el-form-item>
-        <el-form-item label="角色设定" prop="systemMessage">
-          <el-input v-model="form.systemMessage" type="textarea" placeholder="请输入角色设定" />
-        </el-form-item>
-        <el-form-item label="引用知识库" prop="knowledgeIds">
-          <el-select v-model="form.knowledgeIds" placeholder="请选择知识库" clearable multiple>
-            <el-option
-              v-for="item in knowledgeList"
-              :key="item.id"
-              :label="item.name"
-              :value="item.id"
-            />
-          </el-select>
-        </el-form-item>
-        <el-form-item label="引用工具" prop="toolIds">
-          <el-select v-model="form.toolIds" placeholder="请选择工具" clearable multiple>
-            <el-option v-for="item in toolList" :key="item.id" :label="item.name" :value="item.id" />
-          </el-select>
-        </el-form-item>
-        <el-form-item label="引用 MCP" prop="toolIds">
-          <el-select v-model="form.mcpClientNames" placeholder="请选择 MCP" clearable multiple>
-            <el-option
-              v-for="dict in ai_mcp_client_name"
-              :key="dict.value"
-              :label="dict.label"
-              :value="dict.value"
-            />
-          </el-select>
-        </el-form-item>
-        <el-form-item label="是否公开" prop="publicStatus" v-if="!isUser">
-          <el-radio-group v-model="form.publicStatus">
-            <el-radio
-              v-for="dict in boolean_string"
-              :key="dict.value"
-              :value="dict.value"
-            >{{ dict.label }}</el-radio>
-          </el-radio-group>
-        </el-form-item>
-        <el-form-item label="角色排序" prop="sort" v-if="!isUser">
-          <el-input-number v-model="form.sort" :min="0" placeholder="请输入角色排序" />
-        </el-form-item>
-        <el-form-item label="开启状态" prop="status" v-if="!isUser">
-          <el-radio-group v-model="form.status">
-            <el-radio
-              v-for="dict in sys_normal_disable"
-              :key="dict.value"
-              :value="parseInt(dict.value)"
-            >{{ dict.label }}</el-radio>
-          </el-radio-group>
-        </el-form-item>
-      </el-form>
-      <template #footer>
-        <div class="dialog-footer">
-          <el-button type="primary" @click="submitForm">确 定</el-button>
-          <el-button @click="cancel">取 消</el-button>
-        </div>
-      </template>
-    </el-dialog>
+    <!-- 表单弹窗：添加/修改 -->
+    <ChatRoleForm ref="formRef" @success="getList" />
   </div>
 </template>
 
 <script setup name="chatRole">
-import { listRole, getRole, delRole, addRole, updateRole } from "@/api/ai/console/chatRole"
-import { getModelSimpleList } from "@/api/ai/console/model"
-import { AiModelTypeEnum } from '@/utils/constants/AiConstants'
+import { listRole, delRole } from "@/api/ai/console/chatRole"
+import ChatRoleForm from './ChatRoleForm.vue'
 
 const { proxy } = getCurrentInstance()
-const { boolean_string, sys_normal_disable, ai_mcp_client_name } = proxy.useDict('boolean_string', 'sys_normal_disable', 'ai_mcp_client_name')
-
-/** 是否【我】自己创建，私有角色 */
-const isUser = computed(() => {
-  return formType.value === 'my-create' || formType.value === 'my-update'
-})
+const { boolean_string, sys_normal_disable } = proxy.useDict('boolean_string', 'sys_normal_disable')
 
 const roleList = ref([])
-const open = ref(false)
 const loading = ref(true)
 const showSearch = ref(true)
 const total = ref(0)
-const title = ref('')
-const formType = ref('')  // 表单的类型：create - 新增；update - 修改
-const models = ref([])  // 聊天模型列表
-const knowledgeList = ref([]) // 知识库列表
-const toolList = ref([]) // 工具列表
+const formRef = ref() // 表单弹窗引用
 
 const data = reactive({
-  form: {},
   queryParams: {
     pageNum: 1,
     pageSize: 10,
     name: null,
     category: null,
     publicStatus: null
-  },
-  rules: {
-    name: [{ required: true, message: '角色名称不能为空', trigger: 'blur' }],
-    avatar: [{ required: true, message: '角色头像不能为空', trigger: 'blur' }],
-    category: [{ required: true, message: '角色类别不能为空', trigger: 'blur' }],
-    sort: [{ required: true, message: '角色排序不能为空', trigger: 'blur' }],
-    description: [{ required: true, message: '角色描述不能为空', trigger: 'blur' }],
-    systemMessage: [{ required: true, message: '角色设定不能为空', trigger: 'blur' }],
-    publicStatus: [{ required: true, message: '是否公开不能为空', trigger: 'blur' }]
   }
 })
 
-const { queryParams, form, rules } = toRefs(data)
+const { queryParams } = toRefs(data)
 
 /** 查询AI 聊天角色列表 */
 function getList() {
@@ -238,32 +134,6 @@ function getList() {
     total.value = response.total
     loading.value = false
   })
-}
-
-// 取消按钮
-function cancel() {
-  open.value = false
-  reset()
-}
-
-// 表单重置
-function reset() {
-  form.value = {
-    id: null,
-    name: null,
-    avatar: null,
-    category: null,
-    description: null,
-    systemMessage: null,
-    modelId: null,
-    knowledgeIds: null,
-    toolIds: null,
-    mcpClientNames: null,
-    publicStatus: 'true',
-    sort: 1,
-    status: 0
-  }
-  proxy.resetForm("roleRef")
 }
 
 /** 搜索按钮操作 */
@@ -279,44 +149,8 @@ function resetQuery() {
 }
 
 /** 新增/修改按钮操作 */
-function openDialog(type, id, dialogTitle) {
-  reset()
-  formType.value = type
-  title.value = dialogTitle
-  if (id) {
-    getRole(id).then(response => {
-      form.value = response.data
-      form.value.publicStatus = form.value.publicStatus.toString()
-    })
-  }
-  // 获得下拉数据
-  getModelSimpleList(AiModelTypeEnum.CHAT).then(response => {
-    models.value = response.data
-  })
-  // 获取知识库列表
-  // 获取工具列表
-  open.value = true
-}
-
-/** 提交按钮 */
-function submitForm() {
-  proxy.$refs["roleRef"].validate(valid => {
-    if (valid) {
-      if (form.value.id != null) {
-        updateRole(form.value).then(response => {
-          proxy.$modal.msgSuccess("修改成功")
-          open.value = false
-          getList()
-        })
-      } else {
-        addRole(form.value).then(response => {
-          proxy.$modal.msgSuccess("新增成功")
-          open.value = false
-          getList()
-        })
-      }
-    }
-  })
+function openForm(type, id) {
+  formRef.value.openDialog(type, id)
 }
 
 /** 删除按钮操作 */
