f3367f4c8c3981277d33fed5c6e5cfdb8a5e8db1
浮生絮语 <1710647252@qq.com>
2026-03-17 10:09:49 +0800

    图片详情组件增加加载状态

--- src/views/ai/image/components/Main/ImageDetail.vue
+++ src/views/ai/image/components/Main/ImageDetail.vue
@@ -4,33 +4,35 @@
     title="图片详细"
     @close="handleDrawerClose"
   >
-    <!-- 图片预览 -->
-    <div class="image-div">
-      <image-preview
-        :src="detail.picUrl"
-        width="100%"
-        height="100%"
-      />
-    </div>
+    <div v-loading="loading">
+      <!-- 图片预览 -->
+      <div class="image-div">
+        <image-preview
+          :src="detail.picUrl"
+          width="100%"
+          height="100%"
+        />
+      </div>
 
-    <!-- 基础信息 -->
-    <el-descriptions title="基础信息" :column="1" :label-width="100" border>
-      <el-descriptions-item label="提交时间">
-        {{ parseTime(detail.createTime) }}
-      </el-descriptions-item>
-      <el-descriptions-item label="生成时间">
-        {{ parseTime(detail.finishTime) }}
-      </el-descriptions-item>
-      <el-descriptions-item label="模型">
-        {{ detail.model }}({{ detail.height }}x{{ detail.width }})
-      </el-descriptions-item>
-      <el-descriptions-item label="提示词">
-        <div class="prompt-text">{{ detail.prompt }}</div>
-      </el-descriptions-item>
-      <el-descriptions-item label="图片地址">
-        <div class="url-text">{{ detail.picUrl }}</div>
-      </el-descriptions-item>
-    </el-descriptions>
+      <!-- 基础信息 -->
+      <el-descriptions title="基础信息" :column="1" :label-width="100" border>
+        <el-descriptions-item label="提交时间">
+          {{ parseTime(detail.createTime) }}
+        </el-descriptions-item>
+        <el-descriptions-item label="生成时间">
+          {{ parseTime(detail.finishTime) }}
+        </el-descriptions-item>
+        <el-descriptions-item label="模型">
+          {{ detail.model }}({{ detail.height }}x{{ detail.width }})
+        </el-descriptions-item>
+        <el-descriptions-item label="提示词">
+          <div class="prompt-text">{{ detail.prompt }}</div>
+        </el-descriptions-item>
+        <el-descriptions-item label="图片地址">
+          <div class="url-text">{{ detail.picUrl }}</div>
+        </el-descriptions-item>
+      </el-descriptions>
+    </div>
   </el-drawer>
 </template>
 
@@ -39,6 +41,7 @@ import { getImageMy } from '@/api/ai/image'
 
 const showDrawer = ref(false)
 const detail = ref({})
+const loading = ref(false)
 
 const props = defineProps({
   show: {
@@ -64,8 +67,10 @@ watch(() => props.imageId, (newVal, _oldVal) => {
 })
 
 const getImageDetail = async (imageId) => {
+  loading.value = true
   const { data } = await getImageMy(imageId)
   detail.value = data
+  loading.value = false
 }
 
 const handleDrawerClose = () => {
